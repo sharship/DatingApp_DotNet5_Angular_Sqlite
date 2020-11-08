b99461deeba37fca8b84c15ea5f5e7931a45da03
sharkship <mryuchangrui@gmail.com>
2020-11-08 15:42:14 -0500

    set up authentication

---
+++ API/DTOs/ReadDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class ReadDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }        
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,25 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using API.Extensions;
 
 namespace API
 {
     public class Startup
     {
         private readonly IConfiguration _config;
-
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -28,15 +19,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
+            
             services.AddCors();
-
-            services.AddDbContext<DataContext>(options =>
-                {
-                    options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-                }
-            );
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -63,7 +51,8 @@ namespace API
                     .AllowAnyMethod()
                     .WithOrigins("https://localhost:4200")
             );
-
+            
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

72ff8463fccf88154cb7ecb78a29ca7fdf5e685c
sharkship <mryuchangrui@gmail.com>
2021-04-04 12:43:44 -0400

    review services

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,15 +14,22 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddScoped<LogUserActivity>();
-            services.AddScoped<IPhotoService, PhotoService>();
+            #region register services
+            services.AddScoped<ITokenService, TokenService>();
+
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();             
+            #endregion
+
+            #region register repository
+            services.AddScoped<IUserRepository, UserRepository>();                
+            #endregion
+
+            services.AddScoped<LogUserActivity>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             // services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
                 {
                     options.UseSqlite(config.GetConnectionString("DefaultConnection"));

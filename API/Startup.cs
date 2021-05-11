39e3f9dfc0af62ffea2601819041c7d7c6ef8c04
sharkship <mryuchangrui@gmail.com>
2021-05-11 19:38:22 -0400

    before switch to PostGres

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using API.Extensions;
 using API.Middlewares;
@@ -56,11 +55,17 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
